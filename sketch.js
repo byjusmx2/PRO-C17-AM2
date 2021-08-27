bd05bb98c23194ef9973576805da4605748e13cd
byjusmx2 <87038490+byjusmx2@users.noreply.github.com>
2021-08-26 20:27:04 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -63,7 +63,7 @@ function setup() {
   invisibleGround = createSprite(200,190,400,10);
   invisibleGround.visible = false;
   
-  //create Obstacle and Cloud Groups
+  //crear grupos de obstáculos y nubes
   obstaclesGroup = createGroup();
   cloudsGroup = createGroup();
 
@@ -78,19 +78,19 @@ function setup() {
 function draw() {
   
   background(180);
-  //displaying score
+  //mostrar puntuación
   text("Score: "+ score, 500,50);
   
   
   if(gameState === PLAY){
-    //move the 
+    //mover el gameState 
     gameOver.visible = false;
     restart.visible = false;
-    //change the trex animation
+    //cambiar la animación del Trex
       trex.changeAnimation("running", trex_running);
     
     ground.velocityX = -(4 + 3* score/100)
-    //scoring
+    //puntuación
     score = score + Math.round(frameCount/60);
     
     if(score>0 && score%100 === 0){
@@ -101,19 +101,19 @@ function draw() {
       ground.x = ground.width/2;
     }
     
-    //jump when the space key is pressed
+    //hacer que el Trex salte al presionar la barra espaciadora
     if(keyDown("space")&& trex.y >= 100) {
         trex.velocityY = -12;
         jumpSound.play();
     }
     
-    //add gravity
+    //agregar gravedad
     trex.velocityY = trex.velocityY + 0.8
   
-    //spawn the clouds
+    //aparecer las nubes
     spawnClouds();
   
-    //spawn obstacles on the ground
+    //aparecer obstáculos en el suelo
     spawnObstacles();
     
     if(obstaclesGroup.isTouching(trex)){
@@ -127,7 +127,7 @@ function draw() {
    else if (gameState === END) {
       gameOver.visible = true;
       restart.visible = true;
-     //change the trex animation
+     //cambiar la animación del Trex
       trex.changeAnimation("collided", trex_collided);
        
 
@@ -136,7 +136,7 @@ function draw() {
       trex.velocityY = 0
       
      
-      //set lifetime of the game objects so that they are never destroyed
+      //establecer lifetime (ciclo de vida) de los objetos del juego para que no sean destruidos nunca
     obstaclesGroup.setLifetimeEach(-1);
     cloudsGroup.setLifetimeEach(-1);
      
@@ -145,7 +145,7 @@ function draw() {
    }
   
  
-  //stop trex from falling down
+  //evitar que el Trex caiga
   trex.collide(invisibleGround);
   
 
@@ -160,7 +160,7 @@ function spawnObstacles(){
    var obstacle = createSprite(600,165,10,40);
    obstacle.velocityX = -(6 + score/100);
    
-    //generate random obstacles
+    //generar obstáculos al azar
     var rand = Math.round(random(1,6));
     switch(rand) {
       case 1: obstacle.addImage(obstacle1);
@@ -178,17 +178,17 @@ function spawnObstacles(){
       default: break;
     }
    
-    //assign scale and lifetime to the obstacle           
+    //asignar escala y ciclo de vida al obstáculo           
     obstacle.scale = 0.5;
     obstacle.lifetime = 300;
    
-   //add each obstacle to the group
+   //agregar cada obstáculo al grupo
     obstaclesGroup.add(obstacle);
  }
 }
 
 function spawnClouds() {
-  //write code here to spawn the clouds
+  //escribir aquí el código para aparecer las nubes
  if (frameCount % 60 === 0) {
     var cloud = createSprite(600,120,40,10);
     cloud.y = Math.round(random(80,120));
@@ -196,14 +196,14 @@ function spawnClouds() {
     cloud.scale = 0.5;
     cloud.velocityX = -3;
     
-     //assign lifetime to the variable
+     //asignar ciclo de vida a la variable
     cloud.lifetime = 200;
     
-    //adjust the depth
+    //ajustar la profundidad
     cloud.depth = trex.depth;
     trex.depth = trex.depth + 1;
     
-    //add each cloud to the group
+    //agregar cada nube al grupo
     cloudsGroup.add(cloud);
   }
 }
